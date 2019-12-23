08a9e463d32f9680848fbaabc104f82d31eff149
Liu Jianshe <46677028+Liu-Jianshe@users.noreply.github.com>
2019-12-23 20:44:54 +0800

    Update backward_softmax.m

--- nn/backward_softmax.m
+++ nn/backward_softmax.m
@@ -2,4 +2,4 @@ function dLdX = backward_softmax(P, which_dim, dLdP)
 % P = softmax(Y, which_dim)
 
 P = P - eps; % Account for +eps in forward_softmax
-dLdX = ( dLdP - sum(dLdP .* P, which_dim) ) .* P;
+dLdX = ( dLdP - repmat(sum(dLdP .* P, which_dim),1,size(P,2)) ) .* P;
