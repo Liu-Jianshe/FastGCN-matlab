5c14698198ddfc48c865b4c06e057196c1b13155
Liu Jianshe <46677028+Liu-Jianshe@users.noreply.github.com>
2019-12-23 20:46:19 +0800

    Update forward_softmax.m

--- nn/forward_softmax.m
+++ nn/forward_softmax.m
@@ -1,7 +1,7 @@
 function P = forward_softmax(X, which_dim)
-% P = softmax(Y, which_dim)
+% Px = softmax(Y, which_dim)
 
-expX = exp(X - max(X,[],which_dim)); % Use shifts to ensure numerical stability
-P = expX ./ sum(expX, which_dim);
+expX = exp(X - repmat(max(X,[],which_dim),1,size(X,2))); % Use shifts to ensure numerical stability
+P = expX ./ repmat(sum(expX, which_dim),1,size(X,2));
 P = P + eps; % Avoid explicit zero
-
+% Px = softmax(X);
